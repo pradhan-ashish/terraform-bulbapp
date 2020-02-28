79159a1faaa7302aa5577079d99e5a9dae330d6f
Ashish Pradhan <ashish_s98@hotmail.com>
2020-02-27 19:21:51 -0500

    security group for load balancer added

---
+++ sgs-for-loadbalancer.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "default_for_loadbalancer" {
+  name        = "terraform-ecs-nginx-loadbalancer"
+  description = "Allow traffic"
+  vpc_id      = "vpc-e73c129d"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}

33125686ee67b0f13d2bc58a85c1d273381214d2
Ashish Pradhan <ashish_s98@hotmail.com>
2020-02-27 19:28:14 -0500

    sdfas

--- sgs-for-loadbalancer.tf
+++ sgs-for-loadbalancer.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "default_for_loadbalancer" {
+resource "aws_security_group" "default_loadbalancer" {
   name        = "terraform-ecs-nginx-loadbalancer"
   description = "Allow traffic"
   vpc_id      = "vpc-e73c129d"

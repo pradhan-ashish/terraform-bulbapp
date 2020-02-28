681b9a008266997a9b34685babf79dbf8a8b1f4d
Ashish Pradhan <ashish_s98@hotmail.com>
2020-02-27 21:15:16 -0500

    bhayo

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -19,7 +19,7 @@ resource "aws_lb_listener" "default" {
   }
 }
 resource "aws_lb_target_group" "default" {
-  name     = "terraform-springboot-helloapp"
+  name     = "terraform-ecs-nginx"
   port     = 80
   protocol = "HTTP"
   vpc_id   = "vpc-e73c129d"
